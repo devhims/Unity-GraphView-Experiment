b0fa3c32f8ac13fbee77419a5850cc2bc77f27d7
Himanshu Gupta <thinktank.himanshu@gmail.com>
2021-05-29 10:33:45 +0530

    override GetCompatiblePorts

--- Assets/Editor/StoryGraphView.cs
+++ Assets/Editor/StoryGraphView.cs
@@ -100,4 +100,18 @@ public class StoryGraphView : GraphView
     {
         AddElement(CreateStoryNode(nodeName));
     }
+
+    public override List<Port> GetCompatiblePorts(Port startPort, NodeAdapter nodeAdapter)
+    {
+        var compatiblePorts = new List<Port>();
+        ports.ForEach((port) =>
+        {
+            if (startPort != port && startPort.node != port.node)
+            {
+                compatiblePorts.Add(port);
+            }
+        });
+
+        return compatiblePorts;
+    }
 }
